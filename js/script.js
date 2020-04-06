e18db5de21e0601b27a75544745ede9593ef9e6a
Tyler <marsh.tyler5@gmail.com>
2020-04-06 01:06:08 -0400

    deleted some comments

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 // add a global variable to access studentList in
 // local scopes
 const studentList = document.querySelectorAll('.student-item');
@@ -29,20 +12,6 @@ let perPageShowing = 10;
 // use this to append the links so that they appear after the list of students.
 const endOfPage = document.querySelector('.page')
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 // make a showPage function
 // this calculates the number of profiles to show
 
@@ -71,15 +40,6 @@ const showPage = (list, page) => {
    }
    return
 }
-  
-
-
-
-
-/*
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-*/
 
 // create pagination links at the bottom of the page
 
